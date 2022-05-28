06696d4d56f99bb1052ee60a5fdd350a8c9d2bd2
Fatih Kabak <fatihkabak39@hotmail.com>
2022-05-29 02:36:01 +0300

    Add api communication agent

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,18 +1,39 @@
 import axios, { AxiosResponse } from 'axios';
+import { Activity } from '../models/activity';
+
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
+    })
+}
 
 axios.defaults.baseURL = 'http://localhost:5000/api'
 
-const responseBody = (response: AxiosResponse<any>) => response.data;
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+});
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: object) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: object) => axios.put(url, body).then(responseBody),
-    del: (url: string) => axios.delete(url).then(responseBody),
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: T) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: T) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
-    list: () => requests.get('/activities')
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post('/activities', activity),
+    update: (activity: Activity) => requests.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del(`/activities/${id}`)
 }
 
 const agent = {
